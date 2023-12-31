2b6e75733c51e15057abed2aee2076f636a7362b
Rizwan Shaikh <devilt761@gmail.com>
2023-12-31 23:22:03 +0530

    new

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,16 @@
-new window.Vlitejs("#player",{options:{controls:!1,autoplay:!0,playPause:!0,progressBar:!1,time:!0,volume:!1,fullscreen:!1,bigPlay:!1,playsinline:!1,loop:!0,muted:!1,autoHide:!0}});
+new window.Vlitejs("#player", {
+  options: {
+    controls: true,
+    autoplay: true,
+    playPause: false,
+    progressBar: false,
+    time: false,
+    volume: true,
+    fullscreen: false,
+    bigPlay: false,
+    playsinline: false,
+    loop: true,
+    muted: true,
+    autoHide: true
+  }
+});
